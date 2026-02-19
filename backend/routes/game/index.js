c8ae9a99ada983e10a594903b6d57faeba50ade7
Pedro Ezequiel <pedrovela955@gmail.com>
2026-02-19 12:27:19 -0600

    .

--- backend/routes/game/index.js
+++ backend/routes/game/index.js
@@ -2,11 +2,7 @@ const express = require("express")
 const route = express.Router()
 const path = require("path")
 
-<<<<<<< HEAD
 route.get("/inventario/producto", async(req, res) => {
-=======
-route.get("/game/:id", async(req, res) => {
->>>>>>> parent of 555b37c (se borraron rutas inesesarias)
     try {
         res.sendFile(path.join(__dirname, "../../public/game/index.html"))
     } catch (error) {
@@ -14,16 +10,4 @@ route.get("/game/:id", async(req, res) => {
         }
 })
 
-<<<<<<< HEAD
-
-route.post("/inventario/producto", async(req, res) => {
-    try {
-        res.send("hola")
-    } catch (error) {
-        res.send("no funciona")
-        }
-})
-
-=======
->>>>>>> parent of 555b37c (se borraron rutas inesesarias)
 module.exports = route
