768905335234b89a1a411973e84ac1d1368a8531
Pedro Ezequiel <pedrovela955@gmail.com>
2026-02-23 17:49:01 -0600

    se configuro correctamente la ruta delete de producto

--- backend/routes/api/product/index.js
+++ backend/routes/api/product/index.js
@@ -74,13 +74,11 @@ route.put("/api/product/:id",async (req,res) =>{
 route.delete("/api/product/:id", async (req, res) => {
     const {id} = req.params
     const {token} = req.cookies
-    const {...keysToModify} = req.body
     try {
         const doc = await getInventario(token)
-        const {product_list} = doc
-        const product = product_list.filter((item) => item.id == id)
-        const newProductModify = Object.assign(product[0], keysToModify)
-        res.send(newProductModify)
+        doc.product_list = doc.product_list.filter((product) => product.id !== id)
+        await doc.save()
+        res.status(200).json({message: "Se borro el producto"})
     } catch (error) {
         console.log(error)
         res.status(400).send({message: "Existe un erorr", error})
