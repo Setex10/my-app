26e6c174a710d779cb66425d6545946d388d84b8
Pedro Ezequiel <pedrovela955@gmail.com>
2026-02-23 15:06:17 -0600

    Se creo la ruta post y get para producto

--- backend/routes/api/product/index.js
+++ backend/routes/api/product/index.js
@@ -18,7 +18,7 @@ route.get("/api/product/:id",async (req,res) =>{
     }
 })
 
-route.post("/api/product/", async(req, res) => {
+route.post("/api/product", async(req, res) => {
     const {token} = req.cookies
     const {name, description, quantity, price, img_url, unit_price} = req.body
     try {
@@ -47,5 +47,23 @@ route.post("/api/product/", async(req, res) => {
         })
     }
 })
+
+// route.put("/api/product/:id",async (req,res) =>{
+//     const {id} = req.params
+//     const {token} = req.cookies
+//     const {...oo} = req.body
+//     try {
+//         const doc = await getInventario(token)
+//         const {product_list} = doc
+//         const product = product_list.filter((item) => item.id = id) 
+
+//         res.send(oo)
+//     } catch (error) {
+//         console.log(error)
+//         res.status(400).send({message: "Existe un erorr", error})
+//     }
+// })
+
+
 
 module.exports = route
