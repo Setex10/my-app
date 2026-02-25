101d45e58d410a28ef98d0561fd4f28f41ca149e
alejandro ascencio <al07073244@tecmilenio.mx>
2026-02-24 23:09:53 -0600

    se  integro una home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,34 @@
-import Image from "next/image";
-import { Link, BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>Hola</div>
+    <div>
+
+      <div className="header">
+        Inventarios Maestro
+      </div>
+
+      <div className="info">
+
+        <h1>INVENTARIOS MAESTROS</h1>
+
+        <p>
+          Inventarios Maestro es un sistema diseñado para la gestión y control 
+          de inventario de manera eficiente y organizada.
+        </p>
+
+        <p>
+          La plataforma permite visualizar productos, realizar búsquedas 
+          mediante una barra buscadora y administrar existencias en tiempo real.
+        </p>
+
+        <p>
+          Además, cuenta con opciones para registrar compras, consultar 
+          movimientos y mantener actualizado el inventario de la empresa.
+        </p>
+
+      </div>
+
+    </div>
   );
 }
